dada43e8c2f53507ff352f88911e3ffaef5c9e65
Braden Couch <btkc02@gmail.com>
2026-04-08 18:23:27 -0500

    changing to an else on the calculator

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -21,8 +21,27 @@ elif operation == '2': #2 represents subtraction
   output = int(first_number) - int(second_number)
 elif operation == '3': #3 represents multiplication
   output = int(first_number) * int(second_number)
-elif operation == '4': #4 represents division
+else: #4 represents division
   output = int(first_number) / int(second_number)
 
 
 print(f"The result is: {output}")
+
+
+# PSUEDOCODE
+# START
+# GET NUMBER1 = USER INPUT
+# GET NUMBER2 = USER INPUT
+# ADD NUMBER1 AND NUMBER2
+# PRINT RESULT
+# END
+
+# PSUEDOCODE
+# START
+# GET Take a list of strings
+# SET Iteration = 0
+# WHILE Iteration < LENGTH OF LIST
+#   Connect the strings together
+#   INCREMENT Iteration
+# PRINT RESULT
+# END
