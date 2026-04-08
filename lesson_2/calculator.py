59a84b9673fb38a3cb107cd5b58754abd28d98e5
Braden Couch <btkc02@gmail.com>
2026-04-08 18:12:29 -0500

    pushing calculator file.

---
+++ lesson_2/calculator.py
@@ -0,0 +1,28 @@
+print('Welcome to the Calculator!')
+
+# Ask the user for the first number
+print("What's the First Number?")
+first_number = input()
+
+# Ask the user for the second number
+print("What's the Second Number?")
+second_number = input()
+
+# Print the numbers back to the user
+print(f'{first_number} {second_number}')
+
+#ask for the operation
+print('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide')
+operation = input()
+
+if operation == '1':
+  output = int(first_number) + int(second_number)
+elif operation == '2': #2 represents subtraction
+  output = int(first_number) - int(second_number)
+elif operation == '3': #3 represents multiplication
+  output = int(first_number) * int(second_number)
+elif operation == '4': #4 represents division
+  output = int(first_number) / int(second_number)
+
+
+print(f"The result is: {output}")
