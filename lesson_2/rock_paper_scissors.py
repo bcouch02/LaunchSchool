e6a8082b56cf93059da94a9d4413b111374efa55
Braden C <btkc02@gmail.com>
2026-04-29 11:13:43 -0500

    Part 1 of rock paper scissors

---
+++ lesson_2/rock_paper_scissors.py
@@ -0,0 +1,19 @@
+import random
+
+VALID_CHOICES = ['rock', 'paper', 'scissors']
+
+def prompt(message):
+    print(f'==> {message}')
+
+
+    
+prompt(f'Choose one: {", ".join(VALID_CHOICES)}')
+choice = input()
+
+while choice not in VALID_CHOICES:
+    prompt("That's not a valid choice.")
+    choice = input()
+
+computer_choice = random.choice(VALID_CHOICES)
+
+prompt(f'You chose: {choice}', 'Computer chose: {computer_choice}')
