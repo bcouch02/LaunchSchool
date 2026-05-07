504214add6ab86ad94d6ec0b8ee62dd346305b89
Braden Couch <btkc02@gmail.com>
2026-05-06 21:53:45 -0500

    adding practice problems

--- lesson_3/practice_problems.py
+++ lesson_3/practice_problems.py
@@ -83,3 +83,32 @@ def is_color_valid(color):
     
 def is_color_valid(color):
     return color == "blue" or color == "green"
+
+for padding in range(1, 11):
+    print(f'{"-" * padding}The Flintstones Rock!')
+    
+def factors(number):
+    divisor = number
+    result = []
+    while divisor > 0:
+        if number % divisor == 0:
+            result.append(number // divisor)
+        divisor -= 1
+    return result
+
+###########
+
+def add_to_rolling_buffer1(buffer, max_buffer_size, new_element):
+    buffer.append(new_element)
+    if len(buffer) > max_buffer_size:
+        buffer.pop(0)
+    return buffer
+
+def add_to_rolling_buffer2(buffer, max_buffer_size, new_element):
+    buffer = buffer + [new_element]
+    if len(buffer) > max_buffer_size:
+        buffer.pop(0)
+    return buffer
+
+print(0.3 + 0.6)
+print(0.3 + 0.6 == 0.9)
